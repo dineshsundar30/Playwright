c9677ba51e57a866ed9a3bcf5a18ee0227dd8f3e
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2025-06-11 23:10:51 +0530

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,13 @@
-npm init playwright  --> to initiate the playwright project 
+1. npm init playwright  --> to initiate the playwright project 
+
+2. npx playwright test --> this will run the all test in your directory 
+
+3. if you use only in that test npx playwright test will run only those test 
+for example:
+test.only('focus this test', async ({ page }) => {
+  // Run only focused tests in the entire project.
+});
+
+4.  npx playwright test --headed --> this will run script with head mode by default in playwrite is headless
+
+5.  also we can configure that head settings in config file like --> headless : false,
