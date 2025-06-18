27d56a235ccb390ffb3631f337d690fcc594aedd
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2025-06-18 11:55:58 +0530

    Update ClientApp.spec.js

--- PlayWrightAutomation/tests/ClientApp.spec.js
+++ PlayWrightAutomation/tests/ClientApp.spec.js
@@ -13,7 +13,7 @@ test('@Webst Client App login', async ({ page }) => {
    await page.locator("#userPassword").fill("Iamking@000");
    await page.locator("[value='Login']").click();
    await page.waitForLoadState('networkidle');         //--> if that website is useing the services we can use this so that all the services all callled in network successfully
-   await page.locator(".card-body b").first().waitFor();
+   await page.locator(".card-body b").first().waitFor();   // other method to wait for that to load
    const titles = await page.locator(".card-body b").allTextContents();
    console.log(titles); 
    const count = await products.count();
