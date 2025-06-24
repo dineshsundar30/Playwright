e7cac703ff87a06ac7365390fa5fa9dda556cc6d
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2025-06-24 10:24:16 +0530

    Select, Radio buttons

---
+++ PlayWrightAutomation/Workout/TestBasics/UIBasics3.spec.js
@@ -0,0 +1,28 @@
+const {test,expect} = require('@playwright/test');  
+
+ test.only('@Web UI Controls', async ({page})=>
+ {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+
+    // Select dropdown
+    const documentLink = page.locator("[href*='documents-request']");
+    const dropdown = page.locator("select.form-control");
+    await dropdown.selectOption("consult");                    // Here we need to pass the Value
+
+    
+    await page.locator(".radiotextsty").last().click();  // this .last() will select the last element 
+    await page.locator("#okayBtn").click();
+   // await page.pause();                            //this will use to pause the browser
+    
+    console.log(await page.locator(".radiotextsty").last().isChecked());  // in playwright this will just use to print the true or false 
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();     // .toBeChecked() we can use this for assertions whether it's checked
+    await page.locator("#terms").click();                            
+    await expect( page.locator("#terms")).toBeChecked();
+
+    await page.locator("#terms").uncheck();                               // this .uncheck() will check it's unchecked or not but we can use for directly to assert 
+    expect( await page.locator("#terms").isChecked()).toBeFalsy();                  // but we can use .isChecked() it will return true or false then we can use that assert with .toBeFalsy()
+    await expect(documentLink).toHaveAttribute("class","blinkingText");  
+
+})
