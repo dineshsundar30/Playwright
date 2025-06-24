62fdd1d714bdb9b9947859662fffcf411c8832ad
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2025-06-24 11:39:21 +0530

    UIBasics - browser,page,text

--- PlayWrightAutomation/Workout/TestBasics/UIBasics.spec.js
+++ PlayWrightAutomation/Workout/TestBasics/UIBasics.spec.js
@@ -19,14 +19,14 @@ await page.locator('input#username').fill('rahulshettyacademy');
 await page.locator('input#password').fill('akfakfaad');
 
 await page.locator("input[id='signInBtn']").click();
-console.log(await page.locator("[style*='block']").textContent());               //---> extracting the test form 
+console.log(await page.locator("[style*='block']").textContent());               //---> extracting the text form element
 await expect(page.locator("[style*='block']")).toContainText('Incorrect')        //--> assertions
 
 await page.locator('input#password').fill("");
 await page.locator('input#password').fill("learning");
 await signIn.click();                                              // reusing the locator insted of -> await page.locator("#signInBtn").click(); 
 
-console.log(await cardTitles.first().textContent());               //--> to work with multiple element's the 
+console.log(await cardTitles.first().textContent());               //--> to work with multiple element's the where need to take the specfie element from locator
 console.log(await cardTitles.nth(1).textContent());
 
 const allTitles = await cardTitles.allTextContents();            //--> this allTextContents() don't have auto wait like click so if you use this you will get empty array
