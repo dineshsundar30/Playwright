f4ee64794ec3fc013eb976a099303f3a5366fab1
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2026-06-08 21:51:08 +0530

    Update playwright.config.js

--- PlayWrightAutomation/Workout/TestBasics/playwright.config.js
+++ PlayWrightAutomation/Workout/TestBasics/playwright.config.js
@@ -12,7 +12,16 @@ const config = {
 reporter: 'html',
   use: {
 
-    browserName : 'chromium'
+    browserName : 'chromium',
+    headless : false,
+    screenshot: 'on',
+    trace : 'retain-on-failure',   // it will open log only test is faill, if you want trace for all use 'on'
+    
+//for full screen
+  viewport: null,
+  launchOptions: {
+    args: ['--start-maximized'],
+  },
     
   },
 
